b75dcb65df651e717d620455a71c917662aa2935
Cristian <crismonsalve4@gmail.com>
2025-10-16 20:14:48 -0500

    feature(infrastucture): Agregar conexiones externas

---
+++ app/infrastructure/database.py
@@ -0,0 +1,17 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[MONGO_DB_NAME]
+
+def get_db():
+    return db
+
+def get_agent_collection():
+    return db.agents
