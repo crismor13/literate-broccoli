5c9378f0d94cb73ef9654d77b4b76dfacb941c11
Cristian <crismonsalve4@gmail.com>
2025-10-17 15:24:53 -0500

    feature: Agregar CORS

--- app/main.py
+++ app/main.py
@@ -3,11 +3,25 @@ load_dotenv()
 
 from fastapi import FastAPI
 from app.adapters.controllers import agent_controller
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Gestor de Agentes de IA",
     description="Una API para gestionar agentes de IA y su base de conocimiento.",
     version="1.0.0"
+)
+
+# Orígenes permitidos 
+origins = [
+    "http://localhost:3000", # La URL por defecto de Next.js
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],
 )
 
 # Incluir el router de los agentes
